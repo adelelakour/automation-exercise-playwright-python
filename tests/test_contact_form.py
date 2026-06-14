6348ce08cc2ce4dea144cc458b7defca53f0680b
adelelakour <adel.elakour@gmail.com>
2026-06-15 01:55:36 +0300

    fix: Convert image relative path to absolute path

--- tests/test_contact_form.py
+++ tests/test_contact_form.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import  Playwright, Page, expect
 import re
+from pathlib import Path
 
-
-def test_register_user(page:Page):
+def test_contact_form(page:Page):
     page.goto("https://automationexercise.com/")
     page.get_by_role("link", name="Contact us").click()
     page.get_by_placeholder("Name").fill("Adel Elakour")
@@ -10,7 +10,13 @@ def test_register_user(page:Page):
     page.get_by_placeholder("Subject").fill("Refund request")
     page.get_by_placeholder("Your Message Here").fill("I ordered a t-shirt within order number AD323A and bla bla ")
     file_input = page.locator("input[name='upload_file']")
-    file_input.set_input_files("images/shirt.jpg")
+
+    project_root = Path(__file__).resolve().parents[1]
+    image_path = project_root / "images" / "shirt.jpg"
+
+    page.locator("input[name='upload_file']").set_input_files(str(image_path))
+
+
     file_input = page.locator("input[name='submit']").click()
     page.on("dialog", lambda dialog: dialog.accept())
     
