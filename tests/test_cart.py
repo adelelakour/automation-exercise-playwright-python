6e204b8b7b9778beda4bc8b82f75616359024875
adelelakour <adel.elakour@gmail.com>
2026-06-16 19:09:06 +0300

    test: Add verifying quantity in cart

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -14,6 +14,14 @@ def test_add_multiple_products_to_cart(page:Page):
 
     expect(page.locator("#cart_info_table tbody tr")).to_have_count(2)
 
+def test_product_quantity_in_cart(page:Page):
+    page.goto("https://automationexercise.com/")
+    product = page.locator(".col-sm-4").nth(1)
+    product.locator("a[href='/product_details/1']").click()
+    page.locator("input[name='quantity']").fill("3")
+    page.get_by_role("button", name="Add to cart").click()
+    page.locator(".modal-body a[href='/view_cart']").click()
+    expect(page.get_by_role("button", name="3")).to_be_visible()
 
 def test_remove_product_from_cart(page:Page):
 
