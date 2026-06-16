c6d0e14e8a4025007c4b24b18fb3448a4e16175e
adelelakour <adel.elakour@gmail.com>
2026-06-16 09:36:06 +0300

    test: Add remove_product_from_cart test

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -14,3 +14,18 @@ def test_add_multiple_products_to_cart(page:Page):
 
     expect(page.locator("#cart_info_table tbody tr")).to_have_count(2)
 
+
+def test_remove_product_from_cart(page:Page):
+
+    page.goto("https://automationexercise.com/")
+    first_product = page.locator(".col-sm-4").nth(1)
+    first_product.locator(".productinfo a").click()
+    page.get_by_role("button", name="Continue Shopping").click()
+
+    second_product = page.locator(".col-sm-4").nth(4)
+    second_product.locator(".productinfo a").click(force=True)
+    page.locator(".modal-content a[href='/view_cart']").click()
+
+    page.locator(".cart_delete a.cart_quantity_delete").first.click()
+    expect(page.locator("#cart_info_table tbody tr")).to_have_count(1)
+
