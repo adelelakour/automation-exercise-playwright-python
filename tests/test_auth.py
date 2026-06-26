d2cf385fed43e0c30f8a21bd5ec5c3fbaa7d18ad
adelelakour <adel.elakour@gmail.com>
2026-06-26 11:46:34 +0300

    add Test_ContactUs class

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,18 +1,18 @@
 from playwright.sync_api import  Playwright, Page, expect
-from pages.login_page import Login, Signup
+from pages.login_page import Login, Signup, ContactUs
 import random
 
 
 class Test_Login():
 
     def test_login_valid_credentials(self, page:Page):
-        page.goto("https://automationexercise.com/")
+        page.goto("/")
         login = Login(page)
         login.login_process("adel.elakour@gmail.com", "123456789")
         expect(page.get_by_text("Delete Account")).to_be_visible()
 
     def test_login_invalid_credentials(self, page:Page):
-        page.goto("https://automationexercise.com/")
+        page.goto("/")
         login = Login(page)
         login.login_process("adel.elakour@gmail.com", "22558899")
         expect(page.get_by_text("Your email or password is incorrect")).to_be_visible()
@@ -25,11 +25,10 @@ class Test_Login():
         assert eval_msg == "Please fill out this field."
 
 
-
 class Test_Signup():
 
     def test_create_account_valid_data(self, page:Page):
-        page.goto("https://automationexercise.com/login")
+        page.goto("/login")
         signUp = Signup(page)
         rand_num = random.randint(0,3000)
         signUp.create_account("Dodo", f"Madeha{rand_num}@gmail.com", "Mr. ", "123456789", "23", "August", "1992",
@@ -40,11 +39,28 @@ class Test_Signup():
 
 
     def test_create_account_invalid_data(self, page:Page):
-        page.goto("https://automationexercise.com/login")
+        page.goto("/login")
         signUp = Signup(page)
         signUp.create_account_invalid_email("Dodo", "adelgmail.com")
 
         eval_msg = signUp.signup_email.evaluate("element => element.validationMessage")
         assert "Please include an" in eval_msg
+
+class Test_ContactUs():
+
+    def test_contactUs_valid(self, page:Page):
+        page.goto("/contact_us")
+        page.on("dialog", lambda dialog: dialog.accept())
+        contact_us = ContactUs(page)
+        contact_us.test_contactUs_form("adel", "adel.elakour@gmail.com", "Refund Request",
+                                       "Hi, I bought this shirt, but the quality is lower than I expected. Could you please issue me a refund?", "images/shirt.jpg")
+        expect(page.get_by_text("Get In Touch")).to_be_visible()
+
+
+    def test_contactUs_invalid(self, page:Page):
+        page.goto("/contact_us")
+        contact_us = ContactUs(page)
+        contact_us.test_contactUs_form("adel", "adel.elakour@gmail.com", "Refund Request",
+                                       "Hi, I bought this shirt, but the quality is lower than I expected. Could you please issue me a refund?", "images/shirt.jpg")
 
 
